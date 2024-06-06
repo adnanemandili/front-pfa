3c95977917a5d8397bcfb9a3123972032f51a2df
blacktache <adnanemandili@gmail.com>
2024-06-06 15:25:25 +0100

    auth and register forms

---
+++ src/app/app.routes.ts
@@ -0,0 +1,34 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { RegisterComponent } from './components/register/register.component';
+// import { DashboardComponent } from './components/dashboard/dashboard.component';
+// import { StudentsComponent } from './components/students/students.component';
+// import { ProfessorsComponent } from './components/professors/professors.component';
+// import { InternshipsComponent } from './components/internships/internships.component';
+// import { CompaniesComponent } from './components/companies/companies.component';
+// import { TutorsComponent } from './components/tutors/tutors.component';
+// import { InternshipsDatesComponent } from './components/internships-dates/internships-dates.component';
+// import { SkillsComponent } from './components/skills/skills.component';
+// import { PromosComponent } from './components/promos/promos.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'authenticate', component: AuthenticationComponent, children: [
+    { path: '', component: AuthenticationComponent },
+]},
+
+{ path: 'register', component: RegisterComponent, children: [
+    { path: '', component: RegisterComponent },
+]},
+  
+  // { path: 'dashboard', component: DashboardComponent },
+  // { path: 'students', component: StudentsComponent },
+  // { path: 'professors', component: ProfessorsComponent },
+  // { path: 'promos', component: PromosComponent },
+  // { path: 'internships', component: InternshipsComponent },
+  // { path: 'internships-dates', component: InternshipsDatesComponent },
+  // { path: 'skills', component: SkillsComponent },
+  // { path: 'companies', component: CompaniesComponent },
+  // { path: 'tutors', component: TutorsComponent },
+];
